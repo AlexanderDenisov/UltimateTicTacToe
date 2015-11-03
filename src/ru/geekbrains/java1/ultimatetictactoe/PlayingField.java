788afe1e2fe41344d22af50d51504ae9ec9cee2e
AlexanderDenisov <ad060778@gmail.com>
2015-11-04 02:35:10 +0300

    04.11.2015  Создание поля 9х9. Использование Arrays.fill

--- src/ru/geekbrains/java1/ultimatetictactoe/PlayingField.java
+++ src/ru/geekbrains/java1/ultimatetictactoe/PlayingField.java
@@ -2,9 +2,6 @@ package ru.geekbrains.java1.ultimatetictactoe;
 
 import java.util.Arrays;
 
-/**
- * Created by Кирилл on 04.11.2015.
- */
 public class PlayingField {
     public static void main(String[] args) {
 
@@ -13,23 +10,23 @@ public class PlayingField {
 
         char[] playingField = new char[ROW_SIZE * ROW_SIZE];
 
-        for (int i = 0; i < playingField.length; i++) {
+        /*for (int i = 0; i < playingField.length; i++) {
             playingField[i] = BLANK_CELL;
+        }*/
+        Arrays.fill(playingField, BLANK_CELL);
 
-        }
         playingField[80] = 'X';
 
         showPlayingSection(playingField, ROW_SIZE);
     }
 
-    static char showPlayingSection(char[] cell, int rowSize) {
+    static void showPlayingSection(char[] cell, int rowSize) {
         for (int i = 0; i < cell.length; i++) {
             System.out.print(cell[i] + " ");
             if ((i + 1) % rowSize == 0) {
                 System.out.println();
             }
         }
-        return '_';
     }
 }
 

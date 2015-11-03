a3097adec4bea229cee06d13df42f360ebc9a12d
AlexanderDenisov <ad060778@gmail.com>
2015-11-04 02:25:49 +0300

    04.11.2015  Создание поля 9х9.

--- src/ru/geekbrains/java1/ultimatetictactoe/PlayingField.java
+++ src/ru/geekbrains/java1/ultimatetictactoe/PlayingField.java
@@ -9,20 +9,15 @@ public class PlayingField {
     public static void main(String[] args) {
 
         final char BLANK_CELL = '_';
-        final int ROW_SIZE = 3;
-
-        char[] playingSection = new char[ROW_SIZE * ROW_SIZE];
-
-        for (int i = 0; i < playingSection.length; i++) {
-            playingSection[i] = BLANK_CELL;
-        }
+        final int ROW_SIZE = 9;
 
         char[] playingField = new char[ROW_SIZE * ROW_SIZE];
 
         for (int i = 0; i < playingField.length; i++) {
-            playingField[i] = showPlayingSection(playingSection, ROW_SIZE);
+            playingField[i] = BLANK_CELL;
 
         }
+        playingField[80] = 'X';
 
         showPlayingSection(playingField, ROW_SIZE);
     }
@@ -30,7 +25,7 @@ public class PlayingField {
     static char showPlayingSection(char[] cell, int rowSize) {
         for (int i = 0; i < cell.length; i++) {
             System.out.print(cell[i] + " ");
-            if ((i + 1) / rowSize == 3) {
+            if ((i + 1) % rowSize == 0) {
                 System.out.println();
             }
         }

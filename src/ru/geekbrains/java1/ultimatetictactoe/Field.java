675900abba7483e2f1535faf178dde0fb0b27404
AlexanderDenisov <ad060778@gmail.com>
2015-11-12 00:30:39 +0300

    12.11.2015  Создание координат игрового поля

--- src/ru/geekbrains/java1/ultimatetictactoe/Field.java
+++ src/ru/geekbrains/java1/ultimatetictactoe/Field.java
@@ -3,36 +3,38 @@ package ru.geekbrains.java1.ultimatetictactoe;
 
 public class Field {
     private String blankCell;
-    private String coordinates;
     private int fieldSize;
-    public int n;
     private String[][] playingField;
 
     public Field() {
     }
 
-    public void setCoordinates(String coordinates) {
-        this.coordinates = coordinates;
-        System.out.println(coordinates);
+    public void setCoordinates(int fieldSize) {
+        this.fieldSize = fieldSize;
     }
 
-    public String getCoordinates() {
-        return coordinates;
+    public void getHorizontalCoordinates() {
+        System.out.print(" " + " ");
+        for (int i = fieldSize; i > 0; i--) {
+            System.out.print(" " + i + " ");
+        }
     }
 
-    public void setPlayingField() {
-        this.setFieldSize(3);
-        this.setBlankCell("[ ]");
-        playingField = new String[this.getFieldSize()][this.getFieldSize()];
+    public void setPlayingField(String blankCell, int fieldSize) {
+        this.fieldSize = fieldSize;
+        this.blankCell = blankCell;
+        playingField = new String[fieldSize][fieldSize];
         for (int i = 0; i < playingField.length; i++) {
             for (int j = 0; j < playingField.length; j++) {
-                playingField[j][i] = this.getBlankCell();
+                playingField[j][i] = blankCell;
             }
         }
     }
 
     public String[][] getPlayingField() {
+        System.out.println();
         for (int i = 0; i < playingField.length; i++) {
+            System.out.print((i + 1) + " ");
             for (int j = 0; j < playingField.length; j++) {
                 System.out.print(playingField[j][i]);
             }
--- src/ru/geekbrains/java1/ultimatetictactoe/Game.java
+++ src/ru/geekbrains/java1/ultimatetictactoe/Game.java
@@ -3,15 +3,9 @@ package ru.geekbrains.java1.ultimatetictactoe;
 
 public class Game {
     Field coordinates;
-    Field field0;
-    Field field1;
-    Field field2;
-    Field field3;
-    Field field4;
-    Field field5;
-    Field field6;
-    Field field7;
-    Field field8;
+    Field fieldSize;
+    Field blankCell;
+    Field field;
     private static Game instance;
 
     public static Game getInstance() {
@@ -25,38 +19,18 @@ public class Game {
     }
 
     void init() {
+        fieldSize = new Field();
+        fieldSize.setFieldSize(3);
         coordinates = new Field();
-        coordinates.setCoordinates(" a " + " b " + " c " + "\t " + "d " + " e " + " f " + "\t" + " g " + " h " + " i ");
-        field0 = new Field();
-        field1 = new Field();
-        field2 = new Field();
-        field3 = new Field();
-        field4 = new Field();
-        field5 = new Field();
-        field6 = new Field();
-        field7 = new Field();
-        field8 = new Field();
-        field0.setPlayingField();
-        field1.setPlayingField();
-        field2.setPlayingField();
-        field3.setPlayingField();
-        field4.setPlayingField();
-        field5.setPlayingField();
-        field6.setPlayingField();
-        field7.setPlayingField();
-        field8.setPlayingField();
+        coordinates.setCoordinates(fieldSize.getFieldSize());
+        blankCell = new Field();
+        blankCell.setBlankCell("[ ]");
+        field = new Field();
+        field.setPlayingField(blankCell.getBlankCell(), fieldSize.getFieldSize());
     }
 
     void start() {
-        coordinates.getCoordinates();
-        field0.getPlayingField();
-        field1.getPlayingField();
-        field2.getPlayingField();
-        field3.getPlayingField();
-        field4.getPlayingField();
-        field5.getPlayingField();
-        field6.getPlayingField();
-        field7.getPlayingField();
-        field8.getPlayingField();
+        coordinates.getHorizontalCoordinates();
+        field.getPlayingField();
     }
 }

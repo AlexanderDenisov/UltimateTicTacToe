841b6692714bb661a9c28ed33ec4b753874228d7
AlexanderDenisov <ad060778@gmail.com>
2015-11-22 14:36:03 +0300

    22.11.2015  Создание класса GameActivity, изменение класса Game.

--- src/ru/geekbrains/java1/ultimatetictactoe/Game.java
+++ src/ru/geekbrains/java1/ultimatetictactoe/Game.java
@@ -3,9 +3,7 @@ package ru.geekbrains.java1.ultimatetictactoe;
 
 public class Game {
     private static Game instance;
-    public static Field field;
-    public static Computer computer = new Computer();
-    public static Player player = new Player();
+    public static GameActivity gamePlay;
 
     protected static Game getInstance() {
         if (instance == null) {
@@ -18,42 +16,14 @@ public class Game {
     }
 
     public static void init() {
-        field = new Field();
+        gamePlay = new GameActivity();
     }
 
     public static void start() {
-        System.out.println("___КРЕСТИКИ-НОЛИКИ___");
-        field.showField();
-        for (int i = 0; i < 10; i++) {
-            field.setWinner();
-            if (field.setWinner() == 1) {
-                System.out.println("Победили X");
-                System.out.println("___GAME OVER___");
-                break;
-            }
-            if (field.setWinner() == 2) {
-                System.out.println("Победили O");
-                System.out.println("___GAME OVER___");
-                break;
-            } else {
-                System.out.println("___ХОДЯТ КРЕСТИКИ___");
-                field.doShoot(player.getShoot(), Field.Type.X);
-                field.showField();
-                field.setWinner();
-                System.out.println("___ХОДЯТ НОЛИКИ___");
-                field.doShoot(computer.getShoot(), Field.Type.O);
-                field.showField();
-                field.setWinner();
-            }
-        }
-        if (field.setWinner() == 0) {
-            System.out.println("Ничья!");
-        }
+        gamePlay.gamePlaying();
     }
 
     public static void reset() {
-        field.resetField();
-        System.out.println("\n_____НОВАЯ ИГРА______");
-        field.showField();
+        gamePlay.clearField();
     }
 }
---
+++ src/ru/geekbrains/java1/ultimatetictactoe/GameActivity.java
@@ -0,0 +1,58 @@
+package ru.geekbrains.java1.ultimatetictactoe;
+
+
+public class GameActivity {
+    public static Field field = new Field();
+    ;
+    public static Computer computer = new Computer();
+    public static Player player = new Player();
+
+    public static void gamePlaying() {
+        System.out.println("___КРЕСТИКИ-НОЛИКИ___");
+        pause();
+        field.showField();
+        for (int i = 0; i < 10; i++) {
+            field.setWinner();
+            if (field.setWinner() == 1) {
+                System.out.println("Победили X");
+                System.out.println("___GAME OVER___");
+                break;
+            }
+            if (field.setWinner() == 2) {
+                System.out.println("Победили O");
+                System.out.println("___GAME OVER___");
+                break;
+            } else {
+                pause();
+                System.out.println("___ХОДЯТ КРЕСТИКИ___");
+                field.doShoot(player.getShoot(), Field.Type.X);
+                pause();
+                field.showField();
+                field.setWinner();
+                pause();
+                System.out.println("___ХОДЯТ НОЛИКИ___");
+                field.doShoot(computer.getShoot(), Field.Type.O);
+                pause();
+                field.showField();
+                field.setWinner();
+            }
+        }
+        if (field.setWinner() == 0) {
+            System.out.println("Ничья!");
+        }
+    }
+
+    private static void pause() {
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void clearField() {
+        field.resetField();
+        System.out.println("\n_____НОВАЯ ИГРА______");
+        field.showField();
+    }
+}

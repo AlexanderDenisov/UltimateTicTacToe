639f5778b6fbe2b228e9ceeb5f6901cab0e790db
AlexanderDenisov <ad060778@gmail.com>
2016-01-17 00:06:48 +0300

    17.01.2016 Update

---
+++ src/ru/geekbrains/java1/ultimatetictactoe/model/GameInterface.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.java1.ultimatetictactoe.model;
+
+/**
+ * Created by Кирилл on 11.12.2015.
+ */
+public interface GameInterface {
+
+    void gameStart();
+
+    void gamePlaying();
+
+    void gameOver();
+}
